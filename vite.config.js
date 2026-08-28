754ca0b9f00009ba19f562f64dac8a5eac3c0587
kiddokavin <>
2026-08-28 20:34:30 +0530

    Set relative base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/real-estate-/', // Exact GitHub Pages repository name path
+  base: './', // Relative base path ensures asset resolution on GitHub Pages
   server: {
     port: 3000,
     host: true
